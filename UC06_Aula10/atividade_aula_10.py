384069d435eaf28bc599ac0255e242e4004f5050
Vanessa Vals <vanessadeaguiarlima@gmail.com>
2026-03-06 19:38:02 -0300

    Atividade10

--- UC06_Aula10/atividade_aula_10.py
+++ UC06_Aula10/atividade_aula_10.py
@@ -1,14 +1,15 @@
 nome = []
 idade = []
 
+
+nome = str(input(f"Digite um nome: "))
+idade = str(input(f"Digite sua idade: "))
+
+arquivo = (nome, idade)
+
 with open("cadastros.txt", "a") as arquivo:
-   arquivo.write(nome, "-" , idade, "\n")
+ arquivo.write(nome + "-" + idade + "\n") 
 
-
-for i in range(3):
-    nome = str(input(f"Digite um nome: "))
-    idade = int(input(f"Digite sua idade: "))
-   
 
 
 
