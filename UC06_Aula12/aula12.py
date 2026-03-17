076f1cd04555816b92d506666bed262b33251a32
Vanessa Vals <vanessadeaguiarlima@gmail.com>
2026-03-17 19:30:41 -0300

    Meu progresso na UC6

--- UC06_Aula12/aula12.py
+++ UC06_Aula12/aula12.py
@@ -2,9 +2,6 @@
 import pandas as pd #pd é o nome que eu dei a biblioteca
 
 
-nome = []
-idade = []
-altura = []
 
 nome = str(input(f"Digite um nome: "))
 idade = str(input("Digite sua idade: ")),
@@ -20,14 +17,17 @@ dados = {
 # Data.Frame para realizar a criação de um excel para que o pandas consiga utilizar para depois repassar os dados para a planilha
 # to_excel() criar uma nova planilha, pega os dados digitados pelo usuário no DataFrame e gravar na planilha criada
 excel = pd.DataFrame(dados)
-excel.to_excel("UC06_Aula12")
+excel.to_excel("UC06_Aula12.xlsx")
 leitura_excel = pd.read_excel("UC06_Aula12.xlsx")
 nova_linha = len(leitura_excel)
 
-# Agora o tipo de dados coincide: número com número!
-leitura_excel.loc[3, "nome"] = nome
-leitura_excel.at[3, "idade"] = idade
-leitura_excel.at[3, "altura"] = altura
+# Use as variáveis diretas, sem os colchetes do dicionário
+leitura_excel.loc[nova_linha, "nome"] = dados["nome"]
+leitura_excel.loc[nova_linha, "idade"] = dados["idade"]
+leitura_excel.loc[nova_linha, "altura"] = dados["altura"]
+
+# Salvar a planilha (usando o 'r' para evitar erro de caminho)
+leitura_excel.to_excel(r"UC06_Aula12\cadastro_alunos.xlsx", index=False)
 
 
 
